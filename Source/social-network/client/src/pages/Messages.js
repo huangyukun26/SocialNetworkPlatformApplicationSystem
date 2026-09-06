b5a3d9a80da52b8b4f95aa0d3c4f1203c6d5e137
Charli Huang <112479778+huangyukun26@users.noreply.github.com>
2026-09-06 21:09:33 +0800

    Wire original messaging components to demo friend data

--- Source/social-network/client/src/pages/Messages.js
+++ Source/social-network/client/src/pages/Messages.js
@@ -1,27 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import MessageList from '../components/Messages/MessageList';
 import ChatWindow from '../components/Messages/ChatWindow';
 
 const PageContainer = styled.div`
     display: flex;
-    height: calc(100vh - 64px);
-    margin-top: 64px;
+    height: 100vh;
+    background: #fff;
 `;
 
 const Messages = () => {
     const [selectedChat, setSelectedChat] = useState(null);
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        const loadFriends = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/api/friends');
+                setFriends(Array.isArray(response.data) ? response.data : []);
+            } catch (error) {
+                console.error('获取好友列表失败:', error);
+                setFriends([]);
+            }
+        };
+        loadFriends();
+    }, []);
 
     return (
         <PageContainer>
-            <MessageList 
-                onSelect={setSelectedChat}
-                selectedId={selectedChat?._id}
-            />
+            <div style={{ width: 320, borderRight: '1px solid #e8e8e8' }}>
+                <MessageList 
+                    onSelect={setSelectedChat}
+                    selectedId={selectedChat?._id}
+                    friends={friends}
+                />
+            </div>
             {selectedChat ? (
                 <ChatWindow 
-                    chatId={selectedChat._id}
-                    receiver={selectedChat}
+                    friend={selectedChat}
+                    onMessageSent={() => {}}
                 />
             ) : (
                 <div style={{ 
